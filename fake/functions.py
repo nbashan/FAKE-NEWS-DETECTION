e4cb235a0d75b954bd41c518458e07154151865d
נתנאל בשן <nbashan@g.jct.ac.il>
2021-05-03 18:26:49 +0300

    feat- possible to add as many feature as you like

--- fake/functions.py
+++ fake/functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import scipy
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 import numpy as np
+
 
 def get_features(tf, data, data1):
     array_of_words = []
@@ -13,9 +13,13 @@ def get_features(tf, data, data1):
     y = TfidfTransformer().fit_transform(x.toarray())
     return y
 
-def get_X_y(path :str, sheet_name :str, tfidf :TfidfVectorizer, x_feature :int, y_feature :int, label :int):
+
+def get_X_y(path: str, sheet_name: str, tfidf: TfidfVectorizer, features: list[int], label: int):
     df_train = np.array(pd.read_excel(open(path, 'rb'), sheet_name=sheet_name))
-    return get_features(tfidf, df_train[:, [x_feature]], df_train[:, [y_feature]]), df_train[:, [label]].astype('int'), tfidf.get_feature_names()
+    features = [df_train[:, [feature]] for feature in features]
+    return get_features(tfidf, *features), df_train[:, [label]].astype(
+        'int'), tfidf.get_feature_names()
+
 
 def get_score(clf, X_train, y_train, X_test, y_test):
     clf.fit(X_train, np.ravel(y_train))
--- test.py
+++ test.py
@@ -15,11 +15,11 @@ methods = {"svc": LinearSVC(),
            }
 X_train, y_train, voc = functions.get_X_y(path='data_set_1.xlsx', sheet_name='train',
                                           tfidf=TfidfVectorizer(ngram_range=(1, 5), analyzer='word',
-                                                                stop_words='english'), x_feature=0, y_feature=1,
+                                                                stop_words='english'),features=[0,1],
                                           label=2)
 X_dev, y_dev, temp = functions.get_X_y(path='data_set_1.xlsx', sheet_name='dev',
                                        tfidf=TfidfVectorizer(vocabulary=voc, ngram_range=(1, 5), analyzer='word',
-                                                             stop_words='english'), x_feature=0, y_feature=1,
+                                                             stop_words='english'), features= [0,1],
                                        label=2)
 for i in methods:
     print(functions.get_score(clf=methods[i], X_train=X_train, y_train=y_train, X_test=X_dev, y_test=y_dev),
