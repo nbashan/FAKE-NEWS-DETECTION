2f8667a370447b74239b7e2339c672d5bb7c451d
נתנאל בשן <nbashan@g.jct.ac.il>
2021-05-18 14:40:13 +0300

    feat: FOLD FILES

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from classifier.runner import runner
 from setup.setup import setup
 
 if __name__ == '__main__':
-    runner(ngram_range=5,max_features_start=1000,max_features_jump=1000,max_features_end=11000,ngram=False).get_excel_results()
+    for i in range(5):
+        runner(ngram_range=2,max_features_start=1000,max_features_jump=1000,max_features_end=3000,ngram=False,outpath="try" + str(i) + ".xlsx").get_excel_results()
